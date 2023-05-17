9dbe61e0033c53e3069c861d0ae6d75859b667be
Saravana Perumal <104007654+Saravana-kr22@users.noreply.github.com>
2023-05-18 00:15:11 +0530

    Create makehtml.py

---
+++ script/makehtml.py
@@ -0,0 +1,44 @@
+import subprocess
+import os
+
+def file(path):
+    html_file = "path/to/html"
+    #delete the old build
+    cmd_del = "sudo rm -rf build"
+    subprocess.run(cmd_del , shell = True)
+    
+    file_path = os.path.join(path, html_file)
+    os.chdir(path)
+    #copy the old html to desiginated dir
+    cp_cmd = "cp -r ~/src/dir ~/des/path" 
+    subprocess.run(cp_cmd , shell= True)
+    
+    subprocess.run(cmd_del , shell = True)
+    # Pull the latest changes
+    pull_cmd = "git pull"
+    subprocess.run(pull_cmd, shell=True)
+    # create new html file
+    cmd_build = "sudo make html && sudo make html-clusters"
+    subprocess.run(cmd_build, shell=True)
+    
+    new_file = []
+    for filename in os.listdir(file_path):
+        html_path = os.path.join(file_path, filename)
+        new_file.append(html_path)
+    print(new_file)
+
+    old_file = []
+    old_path = "path/to/old/html"
+    for filename in os.listdir(old_path):
+        html_path = os.path.join(old_path, filename)
+        old_file.append(html_path)
+    print (old_file)
+    keys=['app', 'main']
+    file_list = {}
+    
+    for i in range(len(keys)):
+        file_list[keys[i]] = (new_file[i],old_file[i])
+
+    print(file_list)
+
+    return(file_list)
