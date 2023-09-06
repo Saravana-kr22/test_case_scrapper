f7bfed58dfa82abef139f4ab3af9c0b60fd4459a
Saravana Perumal <104007654+Saravana-kr22@users.noreply.github.com>
2023-09-06 12:28:07 +0530

    Update createtable.py

--- script/createtable.py
+++ script/createtable.py
@@ -1,40 +1,86 @@
-from bs4 import BeautifulSoup
+import pandas as pd
+import openpyxl
+from openpyxl.styles import Font
 import re
-from script.create_table import create_df
+
+def create_df(table):
+                        rows = table.find_all('tr')
+                        data = []
+                        col_spans = []
+                        row_spans = []
+                        # to find the rowspan and colspan
+                        for i, row in enumerate(rows):
+                            cells = row.find_all(['th', 'td'])
+                            row_data = []
+                            for j, cell in enumerate(cells):
+                                if cell.has_attr('colspan'):
+                                    col_spans.append((i, j, int(cell['colspan'])))
+                                if cell.has_attr('rowspan'):
+                                    row_spans.append((i, j, int(cell['rowspan'])))
+                                row_data.append(cell.get_text(strip=True))
+                            data.append(row_data)
+                        # Apply colspans to data
+                        for span in col_spans:
+                            i, j, span_size = span
+                            for r in range(1, span_size):
+                                data[i].insert(j+1, '')
+                        # Apply rowspans to data
+                        for span in row_spans:
+                            i, j, span_size = span
+                            for r in range(1, span_size):
+                                data[i+r].insert(j, '')
+
+                        # Convert the data into a pandas dataframe
+                        df = pd.DataFrame(data[1:], columns=data[0])
+
+                        # Replace any None values with an empty string
+                        df = df.fillna('')
+                        # convert the dataframe to dict
+                        data_dict = {}
+                        # convert the dataframe to dict
+                        data_dict = df.to_dict('list')
+                        #print(data_dict)
+                        return(data_dict)
 
 
-def dict_(h1_tags):
+def tc_details (h1_tags,sheet1,a):
+    if a == 0: 
+        head = ["S.no" ,"Test Plan","Cluster Name",	"TC ID","TC Name","TC Full Name"]
+        sheet1.append(head)
+        next_row = sheet1.max_row + 1
+        appended_row = sheet1[next_row]
+
+    # Make the entire row bold
+        for cell in appended_row:
+            cell.font = Font(bold=True)
+    next_row = sheet1.max_row + 1
+    s=0
+    for i in range(len(h1_tags)):
         
-        wdata_dict = {}
-        for i in range(len(h1_tags)-1):
-            h1 = h1_tags[i].text
-            sheet=h1.replace('Cluster Test Plan', '').replace(' ', '').replace('/', '')
-            print(sheet)
-            first_h1 = h1_tags[i]  # Select the first h1 tag in the pair
+        h1 = h1_tags[i].text
+        cluster_name = h1.replace('Cluster Test Plan', '')
+
+        first_h1 = h1_tags[i]  # Select the first h1 tag in the pair
+        if i == (len(h1_tags)-1):
+            second_h1 = False
+        else:
             second_h1 = h1_tags[i+1]  # Select the second h1 tag in the pair
             
-            # Find all h5 tags after the first h1 tag with id starting with the value
-            h5_tags = first_h1.find_all_next('h5', {'id': lambda x: x and x.startswith('_test_procedure')})  
-            #print (h5_tags)
-            
-            #print (h4_tags.text)
-            # list of testprocedure's containing testcase
-            #test_tags = soup.select('h5[id^="_test_procedure"]')
-            result = []
-
-            # Find the h4 tag of the paeticular cluster
+        # Find all h5 tags after the first h1 tag with id starting with the value
+        h5_tags = first_h1.find_all_next('h5', {'id': lambda x: x and x.startswith('_test_procedure')})  
+        result = []
+        if second_h1:
             for h5_tag in h5_tags:
                 #print(h5_tag)
                 if h5_tag.find_previous('h1') == first_h1:
                     if h5_tag.find_next('h1') == second_h1:
                         result.append(h5_tag)
-            p = 2
-            print (result)
+        
+        else:
+            for h5_tag in h5_tags:
+                result.append(h5_tag)
 
-            
-            data_tables = []
-            
-            if result:
+        if result:
                
                 heads = []
                 for h5_tag in result:
@@ -42,26 +88,10 @@ def dict_(h1_tags):
                     headt= h4_tag.text
                     heads.append(headt)
                     index = result.index(h5_tag)
-                    print (index)
-
-                    testcase1 = re.search(r'\[(.*?)\]',headt )
-                    if testcase1:
-                        matched_str = testcase1.group()  # Extract the matched substring
-                        testcase = re.sub(r'\[|\]', '', matched_str)
-
-                    table = h5_tag.find_next('table')
-                    if table:
-
-                        data_dict = create_df(table)
-                        data_tables.append(data_dict)
-                        wdata_dict[testcase] = data_dict
-                    else: 
-                        print("no table no table no table")
-
-                
+                    #print (index)
 
                     
-            else:
+        else:
 
                 h5_tags = first_h1.find_all_next('h6', {'id': lambda x: x and x.startswith('_test_procedure')})  
 
@@ -78,23 +108,32 @@ def dict_(h1_tags):
                     headt= h4_tag.text
                     heads.append(headt)
                     index = result.index(h5_tag)
-                    print (index)
+                    #print (index)
 
-                    testcase1 = re.search(r'\[(.*?)\]',headt )
-                    if testcase1:
+
+        
+        for j in range(len(result)):
+            testcase1 = re.search(r'\[(.*?)\]',heads[j])
+            if testcase1:
                         matched_str = testcase1.group()  # Extract the matched substring
                         testcase = re.sub(r'\[|\]', '', matched_str)
 
-                    table = h5_tag.find_next('table')
+            tc_name = heads[j].split("]", 1)[-1].strip()
 
-                    print(table)
-                    if table:
-                        data_dict = create_df(table)
-                        data_tables.append(data_dict)
-                        wdata_dict[testcase] = data_dict
-                    else:
-                        print("no table no table no table")
-                    
-            #print (data_tables)   
-                
-        return(wdata_dict)
+            if a == 0:
+                tp = "Main-Test-Plan"
+            else:
+                tp  = "App-Cluster" 
+            
+            if i == 0 & j ==0 :
+                n = 1
+            else:
+                n = sheet1.max_row - 1
+            
+
+            value = [n , tp , cluster_name, testcase,tc_name, heads[j] ]
+            print(value)
+            
+
+            sheet1.append(value)
+        
