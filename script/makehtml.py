bf60c8a84d343de5bcfac75143427ee5efd4f065
Saravana Perumal <104007654+Saravana-kr22@users.noreply.github.com>
2023-09-06 12:27:14 +0530

    Update makehtml.py

--- script/makehtml.py
+++ script/makehtml.py
@@ -8,7 +8,7 @@ def file():
     #delete the old build
     
     cmd_del = "sudo rm -rf build"
-    subprocess.run(cmd_del , shell = True)
+    #subprocess.run(cmd_del , shell = True)
     
     file_path = os.path.join(path, html_file)
     build_path = os.path.join(path,"chip-test-plans")
@@ -16,15 +16,15 @@ def file():
     #copy the old html to desiginated dir
     cp_cmd = "cp -r " + build_path + "/build/html/ " + str(Path(__file__).parent.parent)
 
-    subprocess.run(cp_cmd , shell= True)
+    #subprocess.run(cp_cmd , shell= True)
     
-    subprocess.run(cmd_del , shell = True)
+    #subprocess.run(cmd_del , shell = True)
     # Pull the latest changes
     pull_cmd = "git pull"
-    subprocess.run(pull_cmd, shell=True)
+    #subprocess.run(pull_cmd, shell=True)
     # create new html file
     cmd_build = "sudo make html && sudo make html-clusters"
-    subprocess.run(cmd_build, shell=True)
+    #subprocess.run(cmd_build, shell=True)
     
     new_file = []
     for filename in os.listdir(file_path):
